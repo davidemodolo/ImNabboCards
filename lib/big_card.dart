646efbb46d841d7850ed8635a7211b8c60615303
Davide Modolo <davide.modolo98@gmail.com>
2023-12-22 01:26:39 +0100

    little border

--- lib/big_card.dart
+++ lib/big_card.dart
@@ -48,7 +48,7 @@ class _BigNabboCardState extends State<BigNabboCard>
 
   @override
   Widget build(BuildContext context) {
-    const double imgWidth = 800 * 0.7;
+    const double imgWidth = 800 * 0.8;
     const double imgHeight = 1250 * 0.7;
     return Container(
       margin: const EdgeInsets.all(10),
